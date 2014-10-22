e9f99e46c52dc350b15e285c80faf6df6d90fd25
Céline Aussourd <celine.aussourd@gmail.com>
2014-10-22 13:55:22 +0100

    adding users that have the group as additional group

--- recipes/apache_group.rb
+++ recipes/apache_group.rb
@@ -3,17 +3,31 @@
 # Recipe:: apache_group
 #
 
-# gid of the group to which I want to add users (in our case: Developers group)
+# gid of the group to which I want to add users 
+#   see attributes (in our case: Developers group)
 gid = node['easy_deploy']['gid']
+
 users = node['etc']['passwd']
+groups = node['etc']['group']
+
+# list of users that need to be added to apache group
 developers = Array.new
 
-users.each do |id, details|
-	if details["gid"] == gid
+# adding users that have the group as default group
+users.each do |id, user_details|
+	if user_details["gid"] == gid
 		developers << id
 	end
 end
 
+# adding users that have the group as secondary group
+groups.each do |grp, grp_details|
+	if grp_details["gid"] == gid
+		developers << grp_details["members"]
+	end
+end
+
+# modify the apache group on the node
 group "apache" do
   members developers
   action :manage
